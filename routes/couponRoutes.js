c5d6c01294865660c993940f87cd1d916d916e37
pixelcodenow <pixelcodenow@gmail.com>
2025-10-17 11:42:32 +0600

    update coupon again

--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -2,16 +2,51 @@
 const express = require("express");
 const router = express.Router();
 const { createCoupon, applyCoupon, getCoupons, deleteCoupon, updateCoupon } = require("../controllers/couponController");
-
+const adminOnly = require('../middlewares/adminOnly');
+const verifyToken =require("../middlewares/verifyToken")
 // Admin creates a coupon
-router.post("/add", createCoupon);
+router.post("/add", verifyToken, adminOnly, createCoupon);
 
-// User applies a coupon
-router.post("/apply", applyCoupon);
+// User applies a coupon..........................................................
+router.post("/apply",verifyToken, applyCoupon);
 // get coupons
-router.get("/", getCoupons);
+router.get("/", verifyToken, adminOnly, getCoupons);
 // delete Coupon
-router.delete("/:id", deleteCoupon);
-router.put("/:id",updateCoupon)
+router.delete("/:id", verifyToken, deleteCoupon);
+router.put("/:id", verifyToken,updateCoupon)
 
 module.exports = router;
+
+
+
+// const express = require("express");
+// const router = express.Router();
+// const {
+//   createCoupon,
+//   applyCoupon,
+//   getCoupons,
+//   deleteCoupon,
+//   updateCoupon
+// } = require("../controllers/couponController");
+
+// // const adminOnly = require('../middlewares/adminOnly');
+// // const verifyToken = require("../middlewares/verifyToken");
+
+// // 👉 Make all coupon routes public for now
+
+// // Create Coupon (POST)
+// router.post("/add", createCoupon);
+
+// // Apply Coupon (POST)
+// router.post("/apply", applyCoupon);
+
+// // Get All Coupons (GET)
+// router.get("/", getCoupons);
+
+// // Delete Coupon (DELETE)
+// router.delete("/:id", deleteCoupon);
+
+// // Update Coupon (PUT)
+// router.put("/:id", updateCoupon);
+
+// module.exports = router;
