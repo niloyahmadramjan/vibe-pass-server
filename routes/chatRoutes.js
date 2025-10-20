419ed2bd5672ac7c457120a1be9789e5f44e2d6e
shafayat783593 <sshapa17@gmail.com>
2025-10-20 12:30:23 +0600

    add ai chat api

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
+const {
+    getMessages,
+    sendMessage,
+    getAllUsers,
+    markAsRead,
+    aiChatResponse,
+ getAiChat
+
+} = require("../controllers/chatController")
+
+router.get('/messages', getMessages);
+router.post('/send', sendMessage);
+router.get('/users', getAllUsers);
+router.post('/mark-read', markAsRead);
+
+router.post("/ai", aiChatResponse);
+router.get("/history", getAiChat)
 
 
-router.get('/messages', chatController.getMessages);
-router.post('/send', chatController.sendMessage);
-router.get('/users', chatController.getAllUsers);
-router.post('/mark-read', chatController.markAsRead);
+
 
 module.exports = router;
