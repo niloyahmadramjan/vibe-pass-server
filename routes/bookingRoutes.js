07d9e322865511bfad9a2d74799de18b19ee7a79
Zihad-pro <zubaedhasan46@gmail.com>
2025-10-24 19:42:36 +0600

    fixed

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -10,6 +10,8 @@ const {
   getReservedSeats,
   checkBookingExpiry,
   deleteBooking,
+  refundBooking,
+  UpdateRefundBooking,
 } = require('../controllers/bookingController')
 const verifyToken = require('../middlewares/verifyToken')
 const adminOnly = require('../middlewares/adminOnly')
@@ -17,6 +19,12 @@ const adminOnly = require('../middlewares/adminOnly')
 router.post('/booking', createBooking)
 router.get('/booking/:id', bookingData)
 router.get('/bookings/:id', getBookingById)
+
+// refound
+router.patch("/:id/refund", UpdateRefundBooking);
+router.post("/:id/refund", refundBooking);
+
+
 router.delete('/:id', deleteBooking)
 router.get('/my-bookings', getUserBookings)
 router.get('/weekly-bookings', verifyToken, adminOnly, getWeeklyBookings)
